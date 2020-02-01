5a10d0d048a320f8885433b0b289b9ff8aa87d63
tana-gh <yozoranotakaku@gmail.com>
2020-02-01 17:35:45 +0900

    Adding no** to tsconfig

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode'
 
-export const activate = (context: vscode.ExtensionContext) => {
+export const activate = (_context: vscode.ExtensionContext) => {
 }
 
 export const deactivate = () => {
