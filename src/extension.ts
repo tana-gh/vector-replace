6a1b0d423ee6cd51023aa4f7a43dd349078c0c8f
tana-gh <yozoranotakaku@gmail.com>
2020-02-05 21:53:31 +0900

    Adding showView command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,18 @@
 import * as vscode from 'vscode'
+import * as fs     from 'fs'
+import * as path   from 'path'
 
-export const activate = (_context: vscode.ExtensionContext) => {
+export const activate = (context: vscode.ExtensionContext) => {
+    const showView = vscode.commands.registerCommand('vectorreplace.showView', () => {
+        const htmlPath = path.join(context.extensionPath, 'media', 'views', 'vector-replace.html')
+
+        const panel = vscode.window.createWebviewPanel('vectorreplace', 'Vector Replace', vscode.ViewColumn.Beside)
+        fs.readFile(htmlPath, 'utf8', (err, data) => {
+            if (err) throw err
+            panel.webview.html = data
+        })
+    })
+    context.subscriptions.push(showView)
 }
 
 export const deactivate = () => {
