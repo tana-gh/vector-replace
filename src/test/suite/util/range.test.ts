d7c5d6c0c0227442df632716cd705219281a2c9d
tana-gh <yozoranotakaku@gmail.com>
2021-06-02 15:48:15 +0900

    Add selection search #9

--- src/test/suite/util/range.test.ts
+++ src/test/suite/util/range.test.ts
@@ -60,6 +60,6 @@ suite('range util', () => {
 
         const indices = rangeUtil.toIndicesFromPositions(input, positions)
 
-        assert.strictEqual(indices, [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ])
+        assert.deepStrictEqual(indices, [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ])
     })
 })
